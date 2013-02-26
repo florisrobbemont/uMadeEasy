e5613b23a44ea60003036efb6c85c37b794378ec
Floris Robbemont <florisrobbemont@hotmail.nl>
2013-02-26 13:48:06 +0100

    Added CheckEmptyMercurialRepository ation

---
+++ source/uMadeEasy.Actions/Mercurial/CheckEmptyMercurialRepositoryAction.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Lucrasoft ICT Group. All rights reserved. See License.txt in the project root for license information.
+
+using Lucrasoft.uMadeEasy.Core.Generator;
+using Mercurial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucrasoft.uMadeEasy.Actions.Mercurial
+{
+    /// <summary>
+    /// Commits all files from the newly generator project into Mercurial
+    /// </summary>
+    public class CheckEmptyMercurialRepositoryAction : IGeneratorAction
+    {
+        public string ActionName
+        {
+            get { return "Checck empty Mercurial repository"; }
+        }
+
+        public string RollBackMessage
+        {
+            get { return ""; }
+        }
+
+        public bool AllowContinueAfterError
+        {
+            get { return false; }
+        }
+
+        public bool SupportsRollback
+        {
+            get { return false; }
+        }
+
+        public GeneratorActionResult ExecuteAction(GeneratorArguments arguments, Core.InputFields.ActionInputValues values, Dictionary<string, string> parameters)
+        {
+            var location = values.GetString("DestinationFolder");
+
+            var repo = new Repository(location);
+            if (repo.Manifest().Any())
+            {
+                return new GeneratorActionResult(false, "Repository has commits, and is therefor not empty!");
+            }
+
+            return new GeneratorActionResult(true, "");
+        }
+
+        public GeneratorActionResult RollbackAction(GeneratorArguments arguments, Core.InputFields.ActionInputValues values, Dictionary<string, string> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type InputControl
+        {
+            get { return null; }
+        }
+
+        public IEnumerable<string> RequiredInputFields
+        {
+            get { yield return "DestinationFolder"; }
+        }
+    }
+}
